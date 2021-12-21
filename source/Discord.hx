00d750d303ce2e85d4b00603a41338eae45295b6
BushsHaxs <Bushtrain460615@gmail.com>
2021-12-21 12:58:31 -0600

    update to psych engine 0.5.1

--- source/Discord.hx
+++ source/Discord.hx
@@ -44,7 +44,7 @@ class DiscordClient
 			details: "In the Menus",
 			state: null,
 			largeImageKey: 'icon',
-			largeImageText: "Mod Pack Testing"
+			largeImageText: "Psych Engine"
 		});
 	}
 
@@ -80,7 +80,7 @@ class DiscordClient
 			details: details,
 			state: state,
 			largeImageKey: 'icon',
-			largeImageText: "Useless stuff: " + MainMenuState.psychEngineVersion,
+			largeImageText: "Engine Version: " + MainMenuState.psychEngineVersion,
 			smallImageKey : smallImageKey,
 			// Obtained times are in milliseconds so they are divided so Discord can use it
 			startTimestamp : Std.int(startTimestamp / 1000),
